bbd07bc489f534f04daf76735900a02aa375cbcf
NowelTech <177643139+noweltech@users.noreply.github.com>
2025-12-11 13:48:50 +0300

    db path güncellendi

--- Src/Core/Sevval.Application/Constants/GeneralConstants.cs
+++ Src/Core/Sevval.Application/Constants/GeneralConstants.cs
@@ -1,4 +1,4 @@
-﻿namespace Sevval.Application.Constants
+namespace Sevval.Application.Constants
 {
     public static class GeneralConstants
     {
@@ -9,13 +9,6 @@
         //public const string BaseClientUrl = "http://94.73.131.202:8090";
         //public const string ConnectionString = "Data Source=C:\\inetpub\\SevvalBackEnd\\sevvalemlak2.db;Cache=Shared;Mode=ReadWriteCreate";
 
-        //dev-mac
-        public const string BaseUrl = "http://localhost:5400";
-        public const string WwwRootPath = "/Users/murat/Desktop/sevvalEmlak/sevko/Src/Presentation/Sevval.Web/wwwroot";
-        public const string SendEstateConfirmationEmail = "sevvalsiteonay@gmail.com";
-        public const string BaseClientUrl = "http://94.73.131.202:8090"; // Production API
-        public const string ConnectionString = "Data Source=/Users/murat/Desktop/sevvalEmlak/sevko/Src/Presentation/Sevval.Api/sevvalemlak2.db;Cache=Shared;Mode=ReadWriteCreate";
-
         //test
         //public const string BaseUrl = "http://94.73.131.202:8092";
         //public const string BaseClientUrl = "http://94.73.131.202:8090";
@@ -24,11 +17,11 @@
         //public const string ConnectionString = "Data Source=C:\\inetpub\\SevvalBackEnd\\sevvalemlak2.db;Cache=Shared;Mode=ReadWriteCreate";
 
         //dev
-        //public const string BaseUrl = "http://localhost:5096"; // Frontend URL
-        //public const string WwwRootPath = "C:\\Users\\exdel\\source\\repos\\sevvalcomprojesi\\Src\\Presentation\\Sevval.Web\\wwwroot";
-        //public const string SendEstateConfirmationEmail = "kaaanyildz@gmail.com";
-        //public const string BaseClientUrl = "http://localhost:5235"; // API URL
-        //public const string ConnectionString = "Data Source=C:\\Users\\exdel\\source\\repos\\sevvalcomprojesi\\Src\\Presentation\\Sevval.Api\\sevvalemlak2.db;Cache=Shared;Mode=ReadWriteCreate";
+        public const string BaseUrl = "http://localhost:5096"; // Frontend URL
+        public const string WwwRootPath = "C:\\Users\\Dell\\sevvalcomprojesi\\Src\\Presentation\\Sevval.Web\\wwwroot";
+        public const string SendEstateConfirmationEmail = "kaaanyildz@gmail.com";
+        public const string BaseClientUrl = "http://localhost:5235"; // API URL
+        public const string ConnectionString = "Data Source=C:\\Users\\Dell\\sevvalcomprojesi\\Src\\Presentation\\Sevval.Api\\sevvalemlak2.db;Cache=Shared;Mode=ReadWriteCreate";
 
         public const string SecretKey = "SeVVAlEmlaK3300129231321943198419SEVVALEMLAK";
     }
