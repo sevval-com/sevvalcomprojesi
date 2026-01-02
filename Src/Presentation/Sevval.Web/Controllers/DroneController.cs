b57c143d54c3fe659a1b258894dbc2f6014049e7
Berk Subaşı <berksubasi@Berk-MacBook-Pro.local>
2026-01-02 13:37:34 +0300

    Add Sevval Drone pages and integrations

--- Src/Presentation/Sevval.Web/Controllers/DroneController.cs
+++ Src/Presentation/Sevval.Web/Controllers/DroneController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 
 public class DroneController : Controller
 {
+    public IActionResult Index() { return RedirectToAction(nameof(ParselSorgu)); }
     public IActionResult Olustur() { return View(); }
     public IActionResult ParselSorgu() { return View(); }
+    public IActionResult KayitliParseller() { return View(); }
+    public IActionResult ArsaVideolarim() { return View(); }
 }
